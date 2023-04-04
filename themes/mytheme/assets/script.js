bd0c955ba933bef55298d3f3d13cb82c2c38002c
Maxime Herbaut <maximeherbaut@MacBook-Pro-Maxime.local>
2023-04-04 11:06:54 +0200

    enable dark mode toggle

---
+++ themes/mytheme/assets/script.js
@@ -0,0 +1,28 @@
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+
+function disableTransitionsTemporarily() {
+  document.documentElement.classList.add('[&_*]:!transition-none')
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('[&_*]:!transition-none')
+  }, 0)
+}
+
+function toggleMode() {
+  let classes = document.documentElement.classList.value.toString()
+  let dark = classes.includes("dark")
+  console.log(dark)
+  if (dark) {
+    disableTransitionsTemporarily()
+    document.documentElement.classList.remove('dark')
+    localStorage.theme = 'light'
+  } else {
+    disableTransitionsTemporarily()
+    document.documentElement.classList.add('dark')
+    localStorage.theme = 'dark'
+  }
+}
